ba02947df713af3b7bad39d5e5c4e78f45aae54c
Richard Maya <richard.maya94@gmail.com>
2017-07-05 22:51:52 -0500

    Delivered

---
+++ comming-soon/index.php
@@ -0,0 +1,13 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>CEE</title>
+
+</head>
+<body>
+<div style="text-align: center">
+	<img src="cee.jpg" width="80%" />
+</div>
+</body>
+</html>
